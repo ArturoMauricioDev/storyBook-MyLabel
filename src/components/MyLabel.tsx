87589f2a70acbea78adecc01d58d3a743330b412
Arturo <atercerosb@univalle.edu>
2023-02-28 11:12:21 -0400

    storybook - MyLabel

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,41 @@
+import "./mylabel.css";
+
+export interface MyLabelProps {
+  /**
+   * Label contents
+   */
+  label: string;
+  /**
+   * How large should the label be?
+   */
+  size: "normal" | "h1" | "h2" | "h3";
+  /**
+   * What type of color to use?
+   */
+  color?: "primary" | "secondary" | "tertiary";
+  /**
+   * All caps want to see? ?
+   */
+  allCaps?: boolean;
+  /**
+   * What font color to use?
+   */
+  fontColor?: string;
+}
+
+export const MyLabel = ({
+  label = "No Label",
+  size = "normal",
+  color = "primary",
+  allCaps = false,
+  fontColor,
+}: MyLabelProps) => {
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{ color: fontColor }}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
